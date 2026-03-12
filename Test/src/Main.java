f3f6c2914d8a230889095f1b433247ddfec00f47
s33232 <s33232@pjwstk.edu.pl>
2026-03-12 12:57:34 +0100

    Add logic to parse input into integer array

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -5,10 +5,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Wprowadź liczby oddzielone przecinkiem:");
         String input = scanner.nextLine();
-
         if (input == null || input.trim().isEmpty()) {
             System.out.println("Błąd: Wejście nie może być puste.");
-            return;
+        }
+            try {
+                String[] stringNumbers = input.split(",");
+                int[] numbers = new int[stringNumbers.length];
+                for (int i = 0; i < stringNumbers.length; i++) {
+                    numbers[i] = Integer.parseInt(stringNumbers[i].trim());
+                }
+                System.out.println("Wprowadzono " + numbers.length + " liczb.");
+            } catch (NumberFormatException e) {
+                System.out.println("Błąd: Wprowadzono niepoprawne dane. Użyj tylko cyfr i przecinków.");
+            }
         }
     }
-}
